2b5fca8be6297c3107a04577ad4aeb80e367bbdf
Dmitry Dobretsov <berzzzebub@gmail.com>
2023-01-05 15:02:12 +0200

    Added Ajax and WebSockets. Added nice cart.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, { presence: true }
   validates :title, uniqueness: true
   validates :title, allow_blank: true, length: { minimum: 10,  message: 'is too short! Minimum 10 chars.' }
@@ -8,4 +11,11 @@ class Product < ApplicationRecord
   }
   validates :price, { numericality: { greater_than_or_equal_to: 0.01 } }
 
+  private
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line items present.")
+      throw :abort
+    end
+  end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -13,7 +13,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
-    sleep 0.2
+    sleep 2
     get new_product_url
     assert_response :success
   end
@@ -44,7 +44,15 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to product_url(@product)
   end
 
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
+  end
   test "should destroy product" do
+    sleep 3
     assert_difference('Product.count', -1) do
       delete product_url(@product)
     end
